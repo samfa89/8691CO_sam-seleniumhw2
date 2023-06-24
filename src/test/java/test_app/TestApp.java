c3dd097bfff548a2c5e8e0f65b73f09289e43f46
samfa89 <djarounmusta1989@gmail.com>
2023-06-23 21:52:11 -0600

    authentification

--- src/test/java/test_app/TestApp.java
+++ src/test/java/test_app/TestApp.java
@@ -1,17 +1,53 @@
-package test_app;
 
-import app.pom.homepage.Homepage;
-import base.BasePage;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
-public class TestApp extends BasePage {
+public class InvalidCredentialsTest {
+    private WebDriver driver;
 
-    @Test (priority = 0, groups = {"BAT"})
-    public void testNavigationToApplication() {
-        Homepage homepage = new Homepage();
-
-        Assert.assertTrue(isElementVisible(homepage.logo));
+    @BeforeClass
+    public void setUp() {
+        // Set up WebDriver and open the application URL
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://magento.softwaretestingboard.com/");
     }
 
+    @Test(dataProvider = "loginData")
+    public void invalidCredentials(String email, String password) {
+        // Click on "Sign In" link
+        WebElement signInLink = driver.findElement(By.linkText("Sign In"));
+        signInLink.click();
+
+        // Fill in the login form with invalid credentials
+        WebElement emailField = driver.findElement(By.id("email"));
+        WebElement passwordField = driver.findElement(By.id("pass"));
+
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+
+        // Click on "Sign In" button
+        WebElement signInButton = driver.findElement(By.id("send2"));
+        signInButton.click();
+
+        // Verify invalid credentials error message
+        WebElement errorMessage = driver.findElement(By.cssSelector(".message-error"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Expected error message not displayed");
+        Assert.assertEquals(errorMessage.getText(), "Invalid login or password.", "Incorrect error message");
+
+        // Verify user is not logged in
+        WebElement accountDashboard = driver.findElement(By.cssSelector(".dashboard"));
+        Assert.assertFalse(accountDashboard.isDisplayed(), "User was logged in with invalid credentials");
+    }
+
+    @DataProvider(name = "loginData")
+    public Object[][] getLoginData() {
+        return new Object[][]{
+                {"invalid@example.com", "invalidpassword"},
+                // Add more test data here
+        };
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // Close the browser
+        driver.quit();}
 }
